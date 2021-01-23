16aeff959f6faeadab908fa895e0cc2442710296
Adam <adam81501@gmail.com>
2021-01-23 15:06:41 -0500

    Added an audio manager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    #region //Singleton
+    public static AudioManager instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+    #endregion
+
+    public AudioClip[] soundEffects;
+    public AudioClip[] musicTracks;
+
+    public static void PlaySound(int soundIndex)
+    {
+        //create a sound source, play the clip, delete the component
+    }
+
+    public static void PlayMusic(int musicIndex)
+    {
+        //create a sound source, play the clip, delete the component
+    }
+
+    public void onDisable()
+    {
+        instance = null;
+    }
+
+
+}
