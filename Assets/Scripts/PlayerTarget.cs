3310844ba856fdc13da1f2c8dec96bd7fbb44e7e
Adam Stagg <ADStagg@student.fullsail.edu>
2021-01-25 18:52:28 -0500

    Worked on the math and angle functions

--- Assets/Scripts/PlayerTarget.cs
+++ Assets/Scripts/PlayerTarget.cs
@@ -14,25 +14,59 @@ public class PlayerTarget : MonoBehaviour
         }
         set
         {
-            if ( value == null || Vector2.Distance(transform.position, value.transform.position) <= maxDistance )
+            if (value == null || Vector2.Distance(transform.position, value.transform.position) <= maxDistance)
             {
-                try
-                {
+                if (target != null)
                     target.GetComponent<SpriteRenderer>().color = Color.white;
-                } catch
-                {
 
-                }
                 target = value;
-                try
-                {
 
-                target.GetComponent<SpriteRenderer>().color = Color.red;
-                } catch
-                {
-
-                }
+                if (target != null)
+                    target.GetComponent<SpriteRenderer>().color = Color.red;
             }
         }
     }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Debug.Log(GetAngleInRadians());
+        }
+    }
+
+    public float GetAngleInRadians()
+    {
+        Vector3 difference = target.transform.position - transform.position;
+        float angle = Mathf.Atan2(difference.y, difference.x);
+
+        if (angle < 0)
+        {
+            angle += Mathf.PI * 2;
+        } else if (angle > Mathf.PI * 2)
+        {
+            angle -= Mathf.PI * 2;
+        }
+
+        return angle;
+    }
+
+    public int GetAngleSegment()
+    {
+        float angle = GetAngleInRadians();
+
+        int quadrant = (int)(angle % (Mathf.PI / 2));
+
+        for (int i = 0; i < quadrant; i++)
+        {
+            angle -= Mathf.PI / 2;
+        }
+
+
+
+
+        return 0;
+    }
+
+
 }
