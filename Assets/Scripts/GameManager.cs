91f3fcd44d2471356e6cd397fa0734fe2154c820
Thomas Georgiev <TVGeorgiev@student.fullsail.edu>
2021-01-28 21:37:19 -0500

    Added the menu function, working on options menu

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class GameManager : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        Fader.instance.FadeToNextScene();
+
+
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+
+
+    }
+
+    public AudioMixer audioMixer;
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("Volume", Volume);
+    }
+
+}
